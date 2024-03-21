669d8d8e3d8d95e48ed63ecb5047961fae289648
Tawfik Khaled <tawfik.khaled@wizaly.lan>
2024-03-21 21:14:53 +0100

    csv reader

---
+++ 01_regex/main.cpp
@@ -0,0 +1,14 @@
+# include "csv.h"
+#include <iostream>
+
+
+int main(){
+  io::CSVReader<3> in("ram.csv");
+  in.read_header(io::ignore_extra_column, "vendor", "size", "speed");
+  std::string vendor; int size; double speed;
+  while(in.read_row(vendor, size, speed)){
+    std::cout << size;
+    // do stuff with the data
+  }
+  return 0;
+}
