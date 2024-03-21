7aab5c5f24f67fa58fc5a3f6c503c39f85d783eb
Tawfik Khaled <tawfik.khaled@wizaly.lan>
2024-03-21 23:19:45 +0100

    problem leetcode frame

---
+++ 02-leetcode-10-regular-expression-matching/main.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+/*
+run with  : g++ .\main.cpp ; ./a.exe
+*/
+
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        return false;
+    }
+};
+
+int main() {
+    string s = "";
+    string p = "";
+    Solution sol = Solution();
+    std::cout << sol.isMatch(s, p);
+    return 0;
+}
+
+
